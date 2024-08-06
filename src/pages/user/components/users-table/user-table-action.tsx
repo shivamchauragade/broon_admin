e65de4a4a3580e6969cceb173523d65c3c58e61e
shivamchauragade <schauragade567@gmail.com>
2024-08-06 16:02:59 +0530

    test production changes

--- src/pages/user/components/users-table/user-table-action.tsx
+++ src/pages/user/components/users-table/user-table-action.tsx
@@ -4,7 +4,7 @@ export default function UserTableActions() {
   return (
     <div className="flex items-center justify-between gap-2 py-5">
       <div className="flex flex-1 gap-4">
-        <TableSearchInput placeholder="Search  Here" />
+        <TableSearchInput placeholder="Search User Here" />
       </div>
     </div>
   );
